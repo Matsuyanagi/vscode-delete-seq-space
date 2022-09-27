05fd45a95210eea5b52c40a9323ad5ea131f4d78
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2022-09-27 19:48:25 +0900

    commands_map 削除 複数 editor.selections でのループを editor.selection 1回のみにする。 vscode.commands.executeCommand() のコマンド実行が全マルチカーソルで効果を発揮してしまうため。 コマンド名を 'extension.deleteSequentialSpaces' → 'delete-seq-space.deleteSequentialSpaces' としておく

--- src/delete-seq-space.ts
+++ src/delete-seq-space.ts
@@ -1,15 +1,6 @@
 import * as vscode from 'vscode';
 
-const DELETE_SEQ_SPACE = 'extension.deleteSequentialSpaces'
-
-export const commands_map = new Map<string, ( args: any ) => void>( [
-	[ DELETE_SEQ_SPACE, ( args: any ) => { deleteSequentialSpaces( args ) } ]
-] );
-
-
 export function deleteSequentialSpaces( option?: any ) {
-
-	vscode.window.showInformationMessage( 'export function deleteSequentialSpaces() !!' );
 
 	if ( !option ) {
 		option = {}
@@ -19,56 +10,52 @@ export function deleteSequentialSpaces( option?: any ) {
 		vscode.window.showInformationMessage( 'No editor is active' )
 		return
 	}
-	const seqSpaceReg = new RegExp( "[\s]+" );
 
 	editor.edit( ( ed: vscode.TextEditorEdit ) => {
 		// カーソル箇所が普通文字ならカット、空白文字なら連続削除
+		// マルチカーソルは対応しない。executeCommand() を利用すると各コマンドがエディタ全体に効果発揮する。カーソル回数分実行されてしまうため
+		//	それでもマルチカーソルになっている場合、メインのカーソルの次行の状態によって他のカーソルでも同じ挙動がされてしまう
 
-		// 範囲選択、各選択箇所をカット
-		// 選択範囲は逆順に削除するべきでは
-		// 複数範囲指定には対応しなくていいのでは?でも複数行行頭空白削除とかやりたいか
-		editor.selections.forEach( selection => {
+		// メインのカーソルのみ状態を調べる
+		const selection = editor.selection;
 
-			if ( selection.isEmpty ) {
-				const document = editor.document;
-				// カーソルの1文字
+		if ( selection.isEmpty ) {
+			const document = editor.document;
+			// カーソルの1文字
 
-				const pos = new vscode.Range( selection.active, selection.active.translate( 0, 1 ) );
-				const char = document.getText( pos );
-				if ( char == " " || char == "\t" ) {
-					// 次の単語までの連続した空白を削除
+			const pos = new vscode.Range( selection.active, selection.active.translate( 0, 1 ) );
+			const char = document.getText( pos );
+			if ( char == " " || char == "\t" ) {
+				// 次の単語までの連続した空白を削除
+				vscode.commands.executeCommand( 'deleteWordStartRight' );
+			} else if ( document.lineAt( selection.active ).range.end.character == selection.active.character ) {
+				// 行末
+
+				// 次行の1文字目が空白文字かを先に取っておく
+				let flagSpaceLetter = false;
+				if ( selection.active.line < document.lineCount - 1 ) {
+					const nextLineTopChar = document.lineAt( selection.active.line + 1 ).text.charCodeAt( 0 );
+					if ( nextLineTopChar == 0x9 || nextLineTopChar == 0x20 ) {
+						flagSpaceLetter = true;
+					}
+				}
+
+				// 行末コードを削除
+				vscode.commands.executeCommand( 'deleteRight' );
+				if ( flagSpaceLetter ) {
+					// 次行先頭が空白なら続けて空白削除
 					vscode.commands.executeCommand( 'deleteWordStartRight' );
-				} else if ( document.lineAt( selection.active ).range.end.character == selection.active.character ) {
-					// 行末
-
-					// 次行の1文字目が空白文字かを先に取っておく
-					let flagSpaceLetter = false;
-					if ( selection.active.line < document.lineCount - 1 ) {
-						const nextLineTopChar = document.lineAt( selection.active.line + 1 ).text.charCodeAt( 0 );
-						if ( nextLineTopChar == 9 || nextLineTopChar == 0x20 ) {
-							flagSpaceLetter = true;
-						}
-					}
-
-					vscode.commands.executeCommand( 'deleteRight' );
-					if ( flagSpaceLetter ) {
-						vscode.commands.executeCommand( 'deleteWordStartRight' );
-
-					}
-
-				} else {
-					vscode.commands.executeCommand( 'deleteRight' );
 				}
 
 			} else {
-				vscode.commands.executeCommand( 'editor.action.clipboardCutAction' );
+				// 普通の文字なら1文字削除
+				vscode.commands.executeCommand( 'deleteRight' );
 			}
+		} else {
+			// 選択範囲はクリップボードへカット
+			vscode.commands.executeCommand( 'editor.action.clipboardCutAction' );
 		}
-		)
 	} );
-
-
-
 }
 
 
